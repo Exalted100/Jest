b97e94e3d817c6e1799dcb2ac29b8039bb8df8ec
Fawaz Haroun <harounayomikun@gmail.com>
2021-08-29 23:31:42 +0100

    commit

--- sum.test.js
+++ sum.test.js
@@ -4,12 +4,14 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+//Testing objects
 test('object assignment', () => {
   const data = {one: 1};
   data['two'] = 2;
   expect(data).toEqual({one: 1, two: 2});
 });
 
+//Testing null, undefined and falsy values
 test('null', () => {
   const n = null;
   expect(n).toBeNull();
@@ -27,3 +29,101 @@ test('zero', () => {
   expect(z).not.toBeTruthy();
   expect(z).toBeFalsy();
 });
+
+//Testing ranges with numbers
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe and toEqual are equivalent for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
+
+//Testing floating numbers
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           This won't work because of rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+
+//Checking strings against regular expressions
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+//Checking if an array or iterable contains a value
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk',
+];
+
+test('the shopping list has milk on it', () => {
+  expect(shoppingList).toContain('milk');
+  expect(new Set(shoppingList)).toContain('milk');
+});
+
+//If you want to test whether a particular function throws an error when it's called, use toThrow.
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
+
+//Running a callback test
+// test('the data is peanut butter', done => {
+//   function callback(data) {
+//     try {
+//       expect(data).toBe('peanut butter');
+//       done();
+//     } catch (error) {
+//       done(error);
+//     }
+//   }
+
+//   fetchData(callback);
+// });
+
+// Running tests on promises
+// test('the data is peanut butter', () => {
+//   return fetchData().then(data => {
+//     expect(data).toBe('peanut butter');
+//   });
+// });
+
+//Using .resolves() matcher with promises
+// test('the data is peanut butter', () => {
+//   return expect(fetchData()).resolves.toBe('peanut butter');
+// });
+
+//Testing with Async/Await
+// test('the data is peanut butter', async () => {
+//   const data = await fetchData();
+//   expect(data).toBe('peanut butter');
+// });
+
+// test('the fetch fails with an error', async () => {
+//   expect.assertions(1);
+//   try {
+//     await fetchData();
+//   } catch (e) {
+//     expect(e).toMatch('error');
+//   }
+// });
